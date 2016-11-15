20443ef08a74483398750cded14ac86cc359ef46
becll dell <brahim.infg@gmail.com>
2016-11-15 12:40:34 +0100

    the timepicker run => ok!

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
         <link type="text/css" href="css/bootstrap.min.css" />
         <link type="text/css" href="css/bootstrap-timepicker.min.css" />
         <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.min.js"></script>
         <script type="text/javascript" src="js/bootstrap-timepicker.min.js"></script>
     </head>
     <body>
         <div class="input-group bootstrap-timepicker timepicker">
-            <input id="timepicker1" type="text" class="form-control input-small">
+            <input id="timepicker2" type="text" class="form-control input-small">
             <span class="input-group-addon"><i class="glyphicon glyphicon-time"></i></span>
         </div>
  
